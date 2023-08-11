ec0652aab47679065c1d0a1844b93dcb3cfd998f
Ravinder Gupta <ragu@microsoft.com>
2023-08-11 23:37:33 +0530

    d

--- infra/modules/resource-group/variables.tf
+++ infra/modules/resource-group/variables.tf
@@ -1,6 +1,7 @@
 variable "subscription_id" {
   type        = string
   description = "Subscription id to echo"
+  default     = "d7caf0f4-7c69-4c4a-af92-3b52493f74ca"
 }
 
 variable "resource_groups" {
